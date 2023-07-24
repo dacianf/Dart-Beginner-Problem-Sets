a5ef6fd14cd6eeda8ee1b5e366c3bef25b2e04ce
Dacian Florea <dacianflorea99@gmail.com>
2023-07-24 22:15:19 +0300

    Clean project

--- example/basic_programming_problems_using_dart_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:basic_programming_problems_using_dart/basic_programming_problems_using_dart.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
